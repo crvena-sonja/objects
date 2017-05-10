5fb1b733e20ea6134a6bfc6fca83bdf103aa93c3
Avizc <avirzajac@gmail.com>
2017-05-10 15:09:50 -0400

    We finally got it JS syntax whee

--- object-drills.js
+++ object-drills.js
@@ -71,5 +71,72 @@ array.forEach(function(each){
 
 //DRILL 6
 
+/*function decode(secret){
+    let code = secret.charAt(0);
+    if (code=='a'){
+      console.log(secret.charAt(1));
+    }
+    else if (code=='b'){
+      console.log(secret.charAt(2));
+    }
+    else if (code=='c'){
+      console.log(secret.charAt(3));
+    }
+    else if (code=='d'){
+      console.log(secret.charAt(4));
+    }
+    else{
+      console.log(' ');
+    }
+}
+
+function decodeSentence(sentence) {
+    sentence = sentence.split(" ");
+    
+   return sentence.map(decode).join('');
+}*/
+const code = {
+	a: 2,
+	b: 3,
+	c: 4,
+	d: 5
+}
+function decode(secret, code){
+	let decoded = "";
+	//console.log(secret.split(" "));
+	//console.log(code);
+	secret.split(" ").forEach(
+		function(word){
+			//console.log(code);
+			let char = word.charAt(0);
+			let value = code[char];
+			//console.log(char);
+			//console.log(word);
+			//console.log(value);
+			if(value){
+				decoded+=word.charAt(value-1);
+			}
+			else{
+				decoded+=" ";
+			}
+		})
+
+	/*for (let word in secret.split(" ")){
+		console.log(word);
+		//console.log(word.charAt(0));
+		let value = code[word.charAt(0)];
+		//console.log(code);
+		//console.log(value);
+		if(value){
+			decoded+=word.charAt(value-1);
+		}
+		else{
+			decoded+=" ";
+		}
+	}*/
+	return decoded;
+}
+console.log(decode("craft block argon meter bells brown croon droop", code));
+
 
 //DRILL 7
